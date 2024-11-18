a4a147b46b052c69cfa237b1c5fddc5a02662d61
Beatriz <beatriz@fornasaro.com>
2024-11-18 07:42:37 -0500

    auto updates
    oops forgot to push this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/Autos/specimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep/Autos/specimenAuto.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.IntoTheDeep.Autos;
 
-import static org.firstinspires.ftc.teamcode.IntoTheDeep.Subsystems.CuttleExtendo.ExtendoState.INE;
 import static org.firstinspires.ftc.teamcode.IntoTheDeep.Subsystems.CuttleIntake.IntakeState.UP;
-import static org.firstinspires.ftc.teamcode.IntoTheDeep.Subsystems.CuttleOutake.OutakeState.BUCKET_BAR;
 import static org.firstinspires.ftc.teamcode.IntoTheDeep.Subsystems.CuttleSlides.LiftState.IN;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -16,16 +14,12 @@ import com.roboctopi.cuttlefish.queue.PointTask;
 import com.roboctopi.cuttlefish.queue.TaskList;
 import com.roboctopi.cuttlefish.utils.Pose;
 
-import org.firstinspires.ftc.robotcore.external.State;
 import org.firstinspires.ftc.teamcode.IntoTheDeep.Init.CuttleInitOpMode;
-import org.firstinspires.ftc.teamcode.IntoTheDeep.Subsystems.CuttleIntake;
 
 @Autonomous
-public class BlueSpecimen extends CuttleInitOpMode {
+public class specimenAuto extends CuttleInitOpMode {
     RegularlyUsed methods;
     private State currentState;
-    double finalExtendoPos = 0;
-    double finalLiftPos = 0;
     double counter = 0;
     public boolean transfering = false;
 
@@ -57,17 +51,32 @@ public class BlueSpecimen extends CuttleInitOpMode {
         }));
 
         queue.addTask(new CustomTask(() -> {
-            intakingSample();
+            fistSample();
             return true;
         }));
 
         queue.addTask(new CustomTask(() -> {
-            endIntaking();
+            secondSample();
+            return true;
+        }));
+
+        queue.addTask(new CustomTask(() -> {
+            thirdSample();
             return true;
         }));
 
         queue.addTask(new CustomTask(() ->{
             transferSequence();
+            return true;
+        }));
+
+        queue.addTask(new CustomTask(() ->{
+            specimen();
+            return true;
+        }));
+
+        queue.addTask(new CustomTask(() ->{
+            teleOpInit();
             return true;
         }));
     }
@@ -86,53 +95,43 @@ public class BlueSpecimen extends CuttleInitOpMode {
         telemetry.update();
     }
 
-    void transferSequence(){
-        TaskList transfer = new TaskList();
+    void teleOpInit(){
+        TaskList init = new TaskList();
 
-        transfer.addTask(new CustomTask(()->{
+        init.addTask(new CustomTask(()->{
+            outake.readyPos();
+            liftPosition = 0;
+            extendoPosition = 0;
+            intake.initPos();
+            return true;
+        }));
+    }
+
+    void specimen(){
+        TaskList specimen = new TaskList();
+
+        specimen.addTask(new CustomTask(()->{
             intake.intakeDown();
             extendoPosition = 7.5;
             intake.in();
             return true;
         }));
 
-        transfer.addTask(new DelayTask(1500));
+        specimen.addTask(new DelayTask(1500));
 
-        intake.setIntakeState(UP);
-        lift.setLiftState(IN);
-        transfer.addTask(new CustomTask(()->{
 
-            counter += 1;
-            intake.armUp();
-            intake.clawServo.setPosition(0.45);
-            outake.readyPos();
-            extendoPosition = 0;
-            liftPosition = 0;
-            telemetry.addData("tranfer sequence running", true);
-            return true;
-        }));
-        transfer.addTask(new DelayTask(300));
-        transfer.addTask(new CustomTask(()->{
-            //finalSlidePos = extendo.extendoMachine(true, false, false);
-            outake.transferPos();
-            return true;
-        }));
-        transfer.addTask(new DelayTask(200));
-        transfer.addTask(new CustomTask(()->{
-            outake.grippedPos();
-            intake.initPos();
-            intake.setIntakeState(UP);
-            return true;
-        }));
-        transfer.addTask(new DelayTask(200));
-        transfer.addTask(new CustomTask(()->{
+        //scoring
+
+        specimen.addTask(new DelayTask(200));
+        specimen.addTask(new CustomTask(()->{
             outake.autoHighRungPos();
             outake.wristLeft();
             liftPosition = 5.1;
             return true;
         }));
-        transfer.addTask(new DelayTask(200));
-        transfer.addTask(new CustomTask(()->{
+
+        specimen.addTask(new DelayTask(200));
+        specimen.addTask(new CustomTask(()->{
             intake.intakeDown();
             //extendoPosition = 7.5;
             transfering = false;
@@ -146,121 +145,127 @@ public class BlueSpecimen extends CuttleInitOpMode {
             return true;
         }));*/
 
-        transfer.addTask(new PointTask(new Waypoint(new Pose(-200, -390, Math.toRadians(0))), ptpController));
+        specimen.addTask(new PointTask(new Waypoint(new Pose(-200, -390, Math.toRadians(0))), ptpController));
 
-        transfer.addTask(new CustomTask(()->{
+        specimen.addTask(new CustomTask(()->{
             outake.readyPos();
             liftPosition = 0;
+            return true;
+        }));
+
+    }
+
+    void transferSequence(){
+        TaskList transfer = new TaskList();
+
+        intake.setIntakeState(UP);
+        lift.setLiftState(IN);
+
+        transfer.addTask(new CustomTask(()->{
+            counter += 1;
+            intake.armUp();
+            intake.clawServo.setPosition(0.45);
+            outake.readyPos();
+            extendoPosition = 0;
+            liftPosition = 0;
+            telemetry.addData("tranfer sequence running", true);
+            return true;
+        }));
+
+        transfer.addTask(new DelayTask(300));
+        transfer.addTask(new CustomTask(()->{
+            outake.transferPos();
+            return true;
+        }));
+
+        transfer.addTask(new DelayTask(200));
+        transfer.addTask(new CustomTask(()->{
+            outake.grippedPos();
+            intake.initPos();
+            intake.setIntakeState(UP);
             return true;
         }));
 
         queue.addTask(transfer);
     }
 
-    void endIntaking() {
-        TaskList endIntaking = new TaskList();
 
-        /*if(intake.getColor() == CuttleIntake.Color.BLUE){
-            endIntaking.addTask(new CustomTask(()->{
-                detectingBlue(5);
-                return true;
-            }));
-        }
 
-        else{
-            endIntaking.addTask(new DelayTask(200));
-        }*/
+    void thirdSample() {
+        TaskList third = new TaskList();
 
-        endIntaking.addTask(new DelayTask(1300));
-        endIntaking.addTask(new CustomTask(()->{
-            intake.off();
-            return true;
-        }));
+        third.addTask(new PointTask(new Waypoint(new Pose(-1200, -580, Math.toRadians(115))), ptpController));
 
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-700, -600, Math.toRadians(45))), ptpController));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.out();
-            return true;
-        }));
-
-        endIntaking.addTask(new DelayTask(100));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.off();
-            extendoPosition = 0;
-            return true;
-        }));
-
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-900, -580, Math.toRadians(125))), ptpController));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.in();
-            extendoPosition = 7.5;
-            return true;
-        }));
-
-        endIntaking.addTask(new DelayTask(1300));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.in();
-            extendoPosition = 6;
-            return true;
-        }));
-
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-900, -580, Math.toRadians(45))), ptpController));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.out();
-            return true;
-        }));
-
-        endIntaking.addTask(new DelayTask(100));
-
-        endIntaking.addTask(new CustomTask(()->{
-            intake.off();
-            extendoPosition = 0;
-            return true;
-        }));
-
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-1200, -580, Math.toRadians(115))), ptpController));
-
-        endIntaking.addTask(new CustomTask(()->{
+        third.addTask(new CustomTask(()->{
             intake.in();
             extendoPosition = 4.5;
             return true;
         }));
 
-        endIntaking.addTask(new DelayTask(1300));
+        third.addTask(new DelayTask(1300));
 
-        endIntaking.addTask(new CustomTask(()->{
+        third.addTask(new CustomTask(()->{
             extendoPosition = 2;
             return true;
         }));
 
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-1200, -580, Math.toRadians(35))), ptpController));
+        third.addTask(new PointTask(new Waypoint(new Pose(-1200, -580, Math.toRadians(35))), ptpController));
 
-        endIntaking.addTask(new CustomTask(()->{
+        third.addTask(new CustomTask(()->{
             extendoPosition = 6.5;
             intake.out();
             return true;
         }));
 
-        endIntaking.addTask(new DelayTask(100));
+        third.addTask(new DelayTask(100));
 
-        endIntaking.addTask(new CustomTask(()->{
+        third.addTask(new CustomTask(()->{
             intake.off();
             extendoPosition = 0;
             return true;
         }));
 
-        endIntaking.addTask(new PointTask(new Waypoint(new Pose(-350, -350, Math.toRadians(45))), ptpController));
-        queue.addTask(endIntaking);
+        third.addTask(new PointTask(new Waypoint(new Pose(-350, -350, Math.toRadians(45))), ptpController));
+        queue.addTask(third);
+    }
 
+    void secondSample(){
+        TaskList second = new TaskList();
+
+        second.addTask(new PointTask(new Waypoint(new Pose(-900, -580, Math.toRadians(125))), ptpController));
+
+        second.addTask(new CustomTask(()->{
+            intake.in();
+            extendoPosition = 7.5;
+            return true;
+        }));
+
+        second.addTask(new DelayTask(1300));
+
+        second.addTask(new CustomTask(()->{
+            intake.in();
+            extendoPosition = 6;
+            return true;
+        }));
+
+        second.addTask(new PointTask(new Waypoint(new Pose(-900, -580, Math.toRadians(45))), ptpController));
+
+        second.addTask(new CustomTask(()->{
+            intake.out();
+            return true;
+        }));
+
+        second.addTask(new DelayTask(100));
+
+        second.addTask(new CustomTask(()->{
+            intake.off();
+            extendoPosition = 0;
+            return true;
+        }));
     }
 
 
-    void intakingSample(){
+    void fistSample(){
         TaskList intakingSample = new TaskList();
 
         intakingSample.addTask(new PointTask(new Waypoint(new Pose(-700, -600, Math.toRadians(126))), ptpController));
@@ -276,6 +281,27 @@ public class BlueSpecimen extends CuttleInitOpMode {
 
         intakingSample.addTask(new CustomTask(()->{
             extendoPosition = 5.8;
+            return true;
+        }));
+
+        intakingSample.addTask(new DelayTask(1300));
+        intakingSample.addTask(new CustomTask(()->{
+            intake.off();
+            return true;
+        }));
+
+        intakingSample.addTask(new PointTask(new Waypoint(new Pose(-700, -600, Math.toRadians(45))), ptpController));
+
+        intakingSample.addTask(new CustomTask(()->{
+            intake.out();
+            return true;
+        }));
+
+        intakingSample.addTask(new DelayTask(100));
+
+        intakingSample.addTask(new CustomTask(()->{
+            intake.off();
+            extendoPosition = 0;
             return true;
         }));
 
