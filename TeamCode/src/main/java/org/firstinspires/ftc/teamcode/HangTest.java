61cc8fc0b3ac8129e752760d837a93b7aed83dc7
Beatriz <beatriz@fornasaro.com>
2025-01-26 14:46:24 -0500

    I forgot to push this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HangTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class HangTest extends CuttleInitOpModeRobot2 {
+    public void onInit() {
+        super.onInit();
+        hang.initHang();
+    }
+    public void main() {
+        super.main();
+    }
+    public void mainLoop() {
+        super.mainLoop();
+        hang.servoUp();
+        hang.servoDown();
+
+        telemetry.update();
+    }
+}
