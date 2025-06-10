2b8e75cfffe6092d8cc80ae11c91bab862ff47db
Beatriz <beatriz@fornasaro.com>
2025-06-10 19:19:53 -0400

    Revert "this is broken now"
    This reverts commit a71aa37a988f0be5394034bab89b74b251260bfd.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pedro/pedroPathing/constants/FConstants.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.Pedro.pedroPathing.constants;
+
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.localization.Localizers;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FConstants {
+    static {
+        FollowerConstants.localizers = Localizers.PINPOINT;
+
+        FollowerConstants.leftFrontMotorName = "leftFront";
+        FollowerConstants.leftRearMotorName = "leftRear";
+        FollowerConstants.rightFrontMotorName = "rightFront";
+        FollowerConstants.rightRearMotorName = "rightRear";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+    
+        FollowerConstants.mass = 13;
+
+        FollowerConstants.xMovement = 80.6457;
+        FollowerConstants.yMovement = 63.1935;
+
+        FollowerConstants.forwardZeroPowerAcceleration = -27.3312;
+        FollowerConstants.lateralZeroPowerAcceleration = -69.0457;
+
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.07,0,0.01,0);
+        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
+
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(1.95,0,0.1,0);
+        FollowerConstants.useSecondaryHeadingPID = false;
+        FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
+
+        //FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.015,0,0.00017,0.6,0);
+        FollowerConstants.useSecondaryDrivePID = false;
+        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
+
+        FollowerConstants.zeroPowerAccelerationMultiplier = 3;
+        FollowerConstants.centripetalScaling = 0.0004;
+
+        FollowerConstants.pathEndTimeoutConstraint = 500;
+        FollowerConstants.pathEndTValueConstraint = 0.995;
+        FollowerConstants.pathEndVelocityConstraint = 0.1;
+        FollowerConstants.pathEndTranslationalConstraint = 0.1;
+        FollowerConstants.pathEndHeadingConstraint = 0.007;
+    }
+}
